53c2a7562aeb7522a1242b8e98d9243405150d92
Marcelo Arias <hello@marceloarias.com>
2023-03-17 10:23:08 -0500

    Add About screen

---
+++ src/screens/About.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from "react-native";
+
+export default function Result() {
+  return (
+    <View className="flex-1 pt-8 items-center justify-start bg-white dark:bg-slate-800">
+      <View className="w-full flex flex-col justify-center items-center">
+        <Text className="text-lg w-10/12 text-slate-900 dark:text-slate-100 ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam nisl, eget
+          aliquam nisl nisl eget nisl. Donec tincidunt, nisl eget aliquam
+          tincidunt, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.
+          Donec tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam nisl,
+          eget aliquam nisl nisl eget nisl. Donec tincidunt, nisl eget aliquam
+          tincidunt, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.
+          Donec tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam nisl,
+          eget aliquam nisl nisl eget nisl. Donec tincidunt, nisl eget aliquam
+          tincidunt, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.
+          Donec tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam nisl,
+          eget aliquam nisl nisl eget nisl. Donec tincidunt, nisl eget aliquam
+          tincidunt, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
