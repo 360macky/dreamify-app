2430c8e3b2ee78e49f9ac528d52244efb080d69a
Marcelo Arias <hello@marceloarias.com>
2023-03-17 10:22:43 -0500

    Create App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Appearance, useColorScheme } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import Generator from "./src/screens/Generator";
+import Index from "./src/screens/Index";
+import Result from './src/screens/Result';
+import About from './src/screens/About';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  let colorScheme = useColorScheme();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Dreamify"
+          screenOptions={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: colorScheme === "dark" ? "#1e293b" : "#ffffff",
+            },
+            headerTintColor: colorScheme === "dark" ? "#ffffff" : "#1e293b",
+          }}
+        >
+          <Stack.Screen name="Dreamify" component={Index} />
+          <Stack.Screen name="Generator" component={Generator} />
+          <Stack.Screen name="Result" component={Result} />
+          <Stack.Screen name="About" component={About} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
